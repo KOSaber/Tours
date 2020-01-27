9afe4b34c57a6b48eb628894a8afdd21ef99dabc
KOSaber <k.o.saber@hotmail.com>
2020-01-27 14:06:24 +0300

    fixed

--- src/Contact.js
+++ src/Contact.js
@@ -1,31 +1,81 @@
 import React from 'react';
 
 export default function Contact(){
-return(
-    <div>
-        <div className='card'>
-            <h1 class="h12">Abdullah Binmahfouz</h1>
-            <p>Team Leader</p>
-            <p>E-Mail: aaaaaaaaa@gmail.com</p>
+    return (
+        <div>
+          
+          <div>
+                  <div>
+                      <br></br>
+                      <br></br>
+                      <br></br>
+                  </div>
+                  <h1 className="text-center"> Contact Us </h1>
+                  <div>
+                      <br></br>
+                  </div>
+            </div>
+                    
+          {/* Contact */}
+          <div className="contact">
+           
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <div className="contact_title">Get in touch</div>
+                </div>
+              </div>
+              <div className="row contact_content">
+                <div className="col-lg-5">
+                  <div className="contact_text">
+                    <p>.</p>
+                  </div>
+                  <div className="contact_info">
+                    <div className="contact_info_box">i</div>
+                    <div className="contact_info_container">
+                      <div className="contact_info_content">
+                        <ul>
+                          <li>Address: Jeddah, Saudi Arabia</li>
+                          <li>Phone: Only email</li>
+                          <li>Email: contactUs@goldenDevelopers.com</li>
+                        </ul>
+                      </div>
+                      <div className="contact_info_social">
+                        <ul>
+                          {/* <li><a href="#"><i className="fa fa-google-plus" aria-hidden="true" /></a></li>
+                          <li><a href="#"><i className="fa fa-pinterest" aria-hidden="true" /></a></li>
+                          <li><a href="#"><i className="fa fa-facebook" aria-hidden="true" /></a></li>
+                          <li><a href="#"><i className="fa fa-twitter" aria-hidden="true" /></a></li> */}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-7">
+                  <div className="contact_form_container">
+                    <form action="#" id="contact_form" className="clearfix">
+                      <input id="contact_input_name" className="contact_input contact_input_name" type="text" placeholder="Name" required="required" data-error="Name is required." />
+                      <input id="contact_input_email" className="contact_input contact_input_email" type="text" placeholder="E-mail" required="required" data-error="E-mail is required." />
+                      <input id="contact_input_subject" className="contact_input contact_input_subject" type="text" placeholder="Subject" />
+                      <textarea id="contact_input_message" className="contact_message_input contact_input_message" name="message" placeholder="Message" required="required" data-error="Please, write us a message." defaultValue={""} />
+                      <button id="contact_send_btn" type="submit" className="contact_send_btn trans_200" value="Submit">Send</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="row contact_map">
+                {/* Google Map */}
+                <div className="col">
+                  <div id="google_map">
+                    <div className="map_container">
+                      <div id="map" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+           
         </div>
-                
-        <div className='card'>
-            <h1 class="h12">Doaa Turkistani</h1>
-            <p>Team Member</p>
-            <p>E-Mail: doaa.turkistani@hotmail.com</p>
-        </div>
-        
-        <div className='card'>
-            <h1 class="h12">Khadijah Saber</h1>
-            <p>Team Member</p>
-            <p>E-Mail: kkkkkkkkkk@outlook.com</p>
-        </div>
-        
-        <div className='card'>
-            <h1 class="h12">Hanin Nouh</h1>
-            <p>Team Member</p>
-            <p>E-Mail: hhhhhhh@icloud.com</p>
-        </div>
-    </div>
-);
+      );
 }
