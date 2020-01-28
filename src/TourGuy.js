6dc8f00d10d74ea1fcc910a213d94ceb89e8840b
KOSaber <k.o.saber@hotmail.com>
2020-01-28 14:52:29 +0300

    tour guys

--- src/TourGuy.js
+++ src/TourGuy.js
@@ -6,7 +6,69 @@ import { Card } from 'react-bootstrap/';
 function TourGuy() {
   return (
     <div className="TourGuy">
-     <h1>Tour Guys will be here</h1>
+     
+     {/* Page Content */}
+     <div className="container">
+        {/* Jumbotron Header */}
+        <div className="bgpic jumbotron my-4 ">
+        </div>
+        {/* Page Features */}
+        <div className="row text-center">
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="http://placehold.it/600x500" alt="" />
+              <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+              </div>
+              <div className="card-footer">
+                <a href="#" className="btn btn-primary">More!</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="http://placehold.it/600x500" alt="" />
+              <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+              </div>
+              <div className="card-footer">
+                <a href="#" className="btn btn-primary">More!</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="http://placehold.it/600x500" alt="" />
+              <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+              </div>
+              <div className="card-footer">
+                <a href="#" className="btn btn-primary">More!</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="http://placehold.it/600x500" alt="" />
+              <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.</p>
+              </div>
+              <div className="card-footer">
+                <a href="#" className="btn btn-primary">More!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* /.row */}
+      </div>
+
+
+
+
     </div>
   );
 }
