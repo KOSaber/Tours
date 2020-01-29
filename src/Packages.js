16b3eb4c34bd20558b4c0a3b81ec92bcdcd6abf3
Doaa Turkistani <doaa.turkistani@hotmail.com>
2020-01-29 13:15:36 +0300

    finish tour guy profile

--- src/Packages.js
+++ src/Packages.js
@@ -13,7 +13,7 @@ class Packages extends Component {
 
     render() {
      
-        const AllCities=guide.map((item, index) => {
+        const AllPackages=guide.map((item, index) => {
 
           
             return <div key={index} className='Card'>
@@ -25,14 +25,12 @@ class Packages extends Component {
                     <Card.Img variant="top" src={item.imgSrc} width="250" height="250" />
                     <Card.Body>
                     {/* {item.city} */}
-                    <Link to="/TourGayProfile">Package Name</Link>
+                    <Card.Body>Package Name &nbsp; <img src={'https://i.postimg.cc/cHtxQ60w/tour.png'} width="30" height="30" /></Card.Body>
                     <Card.Body>Description about the Package</Card.Body>
-                    <Card.Body>Tour guy name</Card.Body>
-                    <Rater total={5} rating={0} interactive={true} onRate={Event} onRating={true} /> &nbsp;
-                    <Link to="/Comment"> <img src={'https://i.postimg.cc/3NQ9Fmr5/blog.png'} width="30" height="30" /></Link>
+                    <Card.Body>Tour Guy Image</Card.Body>
+                    <Card.Body>Tour Guy Name</Card.Body>
+                    <Link to="/TourGayProfile">Check on His Profile</Link>
                    
-                    {/* <Rater total={5} rating={2} style={{cursor:'pointer'}}/> */}
-                    {/* ,{ react-rater-link:'#ccc' !default} ,{react-rater-hover: #666 !default},{react-rater-active: #000 !default} */}
                     </Card.Body>
                 </Card>
             </div>
@@ -43,15 +41,15 @@ class Packages extends Component {
          <div className='ContainerHomeSearch'>
              <img className='TourGayHomeImg' src={'https://www.miki.co.uk/sites/MikiTravelGlobal/files/slider_One_final.jpg?1580169600073'} width="100%" height="50%"/>
              <div className="searchCont">
-             <p className='HomeText'>Our Best Tours Packages</p>
+             <p className='HomeText'>Our Best Tour Packages</p>
              </div>
              
           </div>  
             <Container>
               
-            <Row>
+            <Row className='Cont'>
                   {/* render the list of city generated in the render method above */}
-                  {AllCities}
+                  {AllPackages}
             </Row>
         </Container>
         </div>
